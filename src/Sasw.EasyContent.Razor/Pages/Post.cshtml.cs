1779ea1e793a9fd6c0f9a9a1bdf3cd9762d6c9f2
Diego Martin <dmsanz@plainconcepts.com>
2020-01-22 17:17:13 +0100

    #2 Created razor class library with basic post viewer

---
+++ src/Sasw.EasyContent.Razor/Pages/Post.cshtml.cs
@@ -0,0 +1,33 @@
+﻿namespace Sasw.EasyContent.Razor.Pages
+{
+    using Configurations;
+    using Contracts.Services;
+    using Microsoft.AspNetCore.Html;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+    using System.Threading.Tasks;
+
+    public class PostModel
+        : PageModel
+    {
+        private readonly IBlogQueryService _blogQueryService;
+        public string Title;
+        public string Author;
+        public HtmlString HtmlContent;
+        public string PublishedOn;
+
+        public PostModel(IBlogQueryService blogQueryService)
+        {
+            _blogQueryService = blogQueryService;
+        }
+
+        public async Task OnGetAsync()
+        {
+            var relativePath = HttpContext.Items[Constants.RequestedPathParamsKey] as string;
+            var post = await _blogQueryService.GetPost(relativePath);
+            Title = post.Title;
+            Author = post.Author;
+            HtmlContent = new HtmlString(post.Content);
+            PublishedOn = post.PublishedOn.ToLongDateString();
+        }
+    }
+}
